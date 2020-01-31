08c097b90b352158ba493768c1250c5613a0e5e6
jotacervanumanni <jota@umanni.com.br>
2020-01-31 13:03:47 -0300

    Remove notes
    Removed NOTES marks and added just the global marks

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,125 +2,106 @@ class Api::V1::UsersController < ApplicationController
   before_action :init_service
   skip_before_action :verify_authenticity_token
 
+  # //MARK: User Create
   def create
     begin
-      # //NOTE: Create: Payload
       payload = { 
         user: user_params.as_json
       }.merge(client_id_secret)
-      # //NOTE: Create: Request
       response = @service.users.post(payload)
 
-      # //NOTE: Create: Check Reponse And RefreshToken
       data = check_response_and_refresh_token(response)
 
-      # //NOTE: Create: Render
       render json: data
     rescue Exception => e
       exception_handler_func(e)
     end
   end
 
+  # //MARK: User Update
   def update
     begin
-      # //NOTE: Create: Payload
       payload = { 
         user: user_params.as_json 
       }
-      # //NOTE: Create: Request
       response = @service.users["/me"].put(payload)
 
-      # //NOTE: Create: Check Reponse
       data = check_response(response)
 
-      # //NOTE: Create: Render
       render json: data
     rescue Exception => e
       exception_handler_func(e)
     end
   end
 
+  # //MARK: User Show Me
   def show_me
     begin
-      # //NOTE: Show Me: Request
       response = @service.users["/me"].get
 
-      # //NOTE: Show Me: Check Reponse
       data = check_response(response)
 
-      # //NOTE: Show Me: Render
       render json: data
     rescue Exception => e
       exception_handler_func(e)
     end
   end
 
+  # //MARK: User Show (ID)
   def show
     begin
-      # //NOTE: Show: Request
       response = @service.users["/#{params[:id]}"].get
 
-      # //NOTE: Show: Check Reponse
       data = check_response(response)
 
-      # //NOTE: Show: Render
       render json: data
     rescue Exception => e
       exception_handler_func(e)
     end
   end
 
+  # //MARK: User Change Password
   def change_password
     begin
-      # //NOTE: Create: Payload
       payload = { 
         user: user_params.as_json 
       }
-      # //NOTE: Create: Request
       response = @service.users["/me/password"].post(payload)
 
-      # //NOTE: Create: Check Reponse
       data = check_response_and_refresh_token(response)
 
-      # //NOTE: Create: Render
       render json: data
     rescue Exception => e
       exception_handler_func(e)
     end
   end
 
+  # //MARK: User Check Email
   def check_email
     begin
-      # //NOTE: Create: Payload
       payload = { 
         email: user_params['email']
       }.merge(client_id_secret)
-      # //NOTE: Create: Request
       response = @service.users["/email"].get({params: payload })
 
-      # //NOTE: Create: Check Reponse
       data = check_response(response)
 
-      # //NOTE: Create: Render
       render json: data
     rescue Exception => e
       exception_handler_func(e)
     end
   end
 
+  # //MARK: User Reset Password
   def reset_password
     begin
-      # //NOTE: Create: Payload
       payload = { 
         user: user_params.as_json 
       }.merge(client_id_secret)
-      # //NOTE: Create: Request
       response = @service.users["/reset_password"].post(payload)
 
-      # //NOTE: Create: Check Reponse
       data = check_response(response)
 
-      # //NOTE: Create: Render
       render json: data
     rescue Exception => e
       exception_handler_func(e)
